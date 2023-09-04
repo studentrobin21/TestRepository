0679cf2bf0c07db2ea25ce3b4c1970d65255aa25
robin <2659865049@qq.com>
2023-09-04 10:11:07 +0800

    templates通过gin启动时不会显示{{define}}和{{end}}，但是通过浏览器打开会显示，因为普通的html无法解析。

--- main/main.go
+++ main/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("zhehsi一个测试文件")
-	fmt.Println("这是第二个测试文件")
-
+	r := gin.Default()
+	r.LoadHTMLGlob("htmls/*")
+	r.Static("/static", "./static")
+	r.GET("/test", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "main.html", gin.H{})
+	})
+	r.GET("/test2", func(c *gin.Context) {
+		c.HTML(200, "header.html", gin.H{})
+	})
+	err := r.Run(":80")
+	if err != nil {
+		return
+	}
 }
